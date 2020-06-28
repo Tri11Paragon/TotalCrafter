6dd0852d48c01ce81c3575b9ba40e2898a98e676
Brett <brettmaster1@gmail.com>
2020-06-28 16:10:27 -0400

    611

--- commit.sh
+++ commit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cd /home/brett/Documents/Java/RMS
 git add *
-git commit -m $1
+if [ -n "$1" ]; then
+	git commit -m $1
+else
+	git commit -m $((1 + RANDOM % 100000))
+fi
 git push -u origin master
